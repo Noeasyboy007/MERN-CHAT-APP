9e5e50dc0823c90aa7b0f81e4807dd247fe82aec
Aritra Bera <aritrabera69@gmail.com>
2024-05-16 18:08:52 +0530

    implement conversations sidebar and useGetConversations Hooks and Zustand Tool For Global State management

--- frontend/src/components/sidebar/Conversation.jsx
+++ frontend/src/components/sidebar/Conversation.jsx
@@ -1,27 +1,32 @@
 
-const Conversation = () => {
+const Conversation = ({conversation, lastIdx, emoji}) => {
+
+    
+
     return (
         <>
             <div className="flex gap-2 items-center hover:bg-purple-700 rounded p-2 py-1 cursor-pointer">
                 <div className="avatar online">
                     <div className='w-12 rounded-full'>
-                        <img src="https://cdn0.iconfinder.com/data/icons/communication-line-10/24/account_profile_user_contact_person_avatar_placeholder-512.png" alt="user avatar" />
+                        <img src={conversation.profilePic}
+                            alt="user avatar"
+                        />
                     </div>
                 </div>
 
 
                 <div className='flex flex-col flex-1'>
                     <div className='flex gap-3 justify-between'>
-                        <p className='font-bold text-gray-200'>Aritra Bera</p>
+                        <p className='font-bold text-gray-200'>{conversation.fullName}</p>
 
-                        <span className='text-xl'>🎃</span>
+                        <span className='text-xl'>{emoji}</span>
                     </div>
 
                 </div>
 
             </div>
 
-            <div className='divider my-0 py-0 h-1' />
+            {!lastIdx && <div className='divider my-0 py-0 h-1' />}
         </>
     )
 }
--- frontend/src/components/sidebar/Conversations.jsx
+++ frontend/src/components/sidebar/Conversations.jsx
@@ -1,13 +1,25 @@
+import useGetConversation from "../../hooks/useGetConversation"
+import { getRandomEmoji } from "../../utils/emojis";
 import Conversation from "./Conversation"
 
 const Conversations = () => {
+
+    const { loading, conversations } = useGetConversation();
+    // console.log("CONVERSATIONS :-", conversations);
+
     return (
         <div className='py-2 flex flex-col overflow-auto'>
-            <Conversation />
-            <Conversation />
-            <Conversation />
-            <Conversation />
-            <Conversation />
+
+            {conversations.map((conversation, idx) => (
+                <Conversation
+                    key={conversation._id}
+                    conversation={conversation}
+                    emoji={getRandomEmoji()}
+                    lastIdx={idx === conversations.length - 1}
+                />
+            ))}
+
+            {loading ? <span className="loading loading-spinner mx-auto" ></span> : null}
         </div>
     )
 }
@@ -17,7 +29,7 @@ export default Conversations
 
 
 
-// STRATER CODE SNIPPET 
+// STRATER CODE SNIPPET
 
 // import Conversation from "./Conversation"
 
