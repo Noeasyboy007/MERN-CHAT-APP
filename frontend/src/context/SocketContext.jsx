09851b23d50942c704123213a710041e0259f744
Aritra Bera <aritrabera69@gmail.com>
2024-05-18 00:30:07 +0530

    -----

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
 	useEffect(() => {
 		if (authUser) {
-			const socket = io("https://chat-app-9rrt.onrender.com", {
+			const socket = io("http://localhost:8000", {
 				query: {
 					userId: authUser._id,
 				},
